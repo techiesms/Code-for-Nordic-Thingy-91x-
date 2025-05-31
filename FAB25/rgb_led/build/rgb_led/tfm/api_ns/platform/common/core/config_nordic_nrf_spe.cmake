afb3e1e96ce45e2fd311bb7864af67c0bc6f135a
Sachin Soni <sachinsms1990@gmail.com>
2025-05-31 22:24:53 +0530

    code

--- FAB25/rgb_led/build/rgb_led/tfm/api_ns/platform/common/core/config_nordic_nrf_spe.cmake
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (c) 2023, Nordic Semiconductor ASA.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-
-set(HAL_NORDIC_PATH C:/ncs/v2.9.1/modules/hal/nordic CACHE STRING "Path to nordic HAL" FORCE)
